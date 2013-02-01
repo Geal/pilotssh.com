da6d241146554abcedce91e430c2a04113e6fdde
Geoffroy Couprie <geo.couprie@gmail.com>
2013-02-01 17:17:24 +0100

    track outbound links in google analytics

--- js/init.js
+++ js/init.js
@@ -7,4 +7,13 @@ $(function(){
 				scrollTop: $($(this).children("a").attr("href")).offset().top
 		 },1500);
 	 });
+  //track outbound links
+  var a = document.getElementsByTagName('a');
+  for(i = 0; i < a.length; i++){
+    if (a[i].href.indexOf(location.host) == -1 && a[i].href.match(/^http:\/\//i)){
+      a[i].onclick = function(){
+        _gaq.push(['_trackEvent', 'outgoing_links', this.href.replace(/^http:\/\//i, '')]);
+      }
+    }
+  }
  });
